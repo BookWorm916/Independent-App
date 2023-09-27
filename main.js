755b2e126f1f70e8384fa6948d08ff8ee15312e9
BookWorm916 <111474572+BookWorm916@users.noreply.github.com>
2023-09-26 19:01:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function logout()
+{
+   window.location = "index.html";
+}
+function chatbot()
+{
+   window.location = "chat.html";
+}
+function shopping()
+{
+   window.location = "shopping.html";
+}
+function game()
+{
+   window.location = "game.html";
+}
---
+++ shopping.js
@@ -0,0 +1,16 @@
+function logout()
+{
+   window.location = "main.html";
+}
+function electronics()
+{
+   window.location = "elect.html";
+}
+function office()
+{
+   window.location = "office.html";
+}
+function clothes()
+{
+   window.location = "clothes.html";
+}
